e53ad76982b9df46e9d44ce43ed8e80ae3af0b52
fishku <juerui@vip.qq.com>
2020-04-17 22:31:31 +0800

    1-初始化gulp任务

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// js压缩 混淆
+function js () {
+  console.log('this is js scripts task')
+}
+
+// scss/less 编译、压缩、输出css文件
+function css () {
+
+}
+
+// 监听这些文件的变化
+function watcher () {
+
+}
+
+// 删除dist目录
+function clean () {
+
+}
+
+// 创建 gulp task
+exports.default = function () {
+  console.log('hello gulp')
+}
+
+// 运行任务 npx gulp --tasks
